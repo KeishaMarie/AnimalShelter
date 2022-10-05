c46142d459dad8a1a823b826c53bbfa9d40218f0
Keisha Hepner <kmhepner90@gmail.com>
2022-10-04 17:02:44 -0700

    Fix MySql connection issues

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -4,10 +4,10 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
-    public int AnimalId { get; set; }
+    public int Id { get; set; }
     public string Type { get; set; }
     public string Name { get; set; }
     public string Breed { get; set; }
-    public DateTime AdmitDate { get; set; }
+    public DateTime Admit_Date { get; set; }
   }
 }
